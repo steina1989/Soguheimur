21121a261170ac26855649e205f798e7b570d19b
Oli <mrdeerhurst@gmail.com>
2017-10-31 15:47:10 +0000

    Part a) designed to Ebbas specifications

--- src/main/resources/Tester/SmokeTest.java
+++ src/main/resources/Tester/SmokeTest.java
@@ -1,4 +1,27 @@
 
+import is.hi.soguheimur.controller.ComposeController;
+import static org.assertj.comre.api.Assertions.assertThat;
+import org.junit.Test;
+import org.junit.runner.Runwith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class) // creates the testing enviorment and runs test with the class Springrunner.class
+@SpringBootTest // all the context is established
+
 public class SmokeTest {
+
+@Autowired
+ComposeController composeController;
+
+/* 
+ * Aðferð til að athuga hvort controllerin hefur verið búinn til
+*/
+
+@Test // defines it as a testing method
+public void contextLoads() {
+	assertThat(composeController).isNotNull(); // Tests to see if controller has been created
+}
 
 }
