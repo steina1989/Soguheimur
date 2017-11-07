5d091b542fc51e0534e9f4e9e14fe49fa328e774
Oli <mrdeerhurst@gmail.com>
2017-11-07 14:57:20 +0000

    Fixed The Bcrypt problem

--- src/main/java/is/hi/soguheimur/services/PasswordEncoder.java
+++ src/main/java/is/hi/soguheimur/services/PasswordEncoder.java
@@ -1,33 +1,39 @@
 package is.hi.soguheimur.services;
 
+import static org.mockito.Matchers.matches;
 
+import org.springframework.security.crypto.bcrypt.BCrypt;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-// Bcript is better than moste available encoders because it is too slow to be affected by brute force attacks within.
+
+// Bcript is better than most available encoders because it is too slow to be affected by brute force attacks within.
 
 public class PasswordEncoder { // this main class is strictly for testing purposes, serves no other use
 	public static void main(String [] args) {
 		String UserPassword="Monkey";
 		System.out.println("Password as entered by user : "+UserPassword);
-		String EncodedPassword = createEncodedPassword(UserPassword);
-		String newEncodedPassword = createEncodedPassword(UserPassword);
+		CharSequence EncodedPassword = createEncodedPassword(UserPassword);
+		CharSequence newEncodedPassword = createEncodedPassword(UserPassword);
 		System.out.println( "Password after Encription printed once : "+EncodedPassword);
 		System.out.println( "Password after Encription printed twice : "+newEncodedPassword);
-		boolean confirmPasword = confirmPassword(UserPassword,UserPassword);
-		System.out.println( "Password confirmation : "+confirmPasword);				
+		boolean confirmaPasword = confirmPassword(UserPassword,createEncodedPassword(UserPassword));
+		System.out.println( "Password confirmation : "+confirmaPasword);				
 	}
 
-public static String createEncodedPassword( String userPassword) {
-	BCryptPasswordEncoder encoder = new BCryptPasswordEncoder(12); // decides the length and strength of password
-	String Password = encoder.encode(userPassword); // encripting the string input from user
-	// System.out.println(Password);
-	return Password;
-}
-public static boolean confirmPassword(String passwordFromUser, String passwordFromDatabase){
-	String passwordEntered = createEncodedPassword(passwordFromUser);
-	if(passwordEntered == passwordFromDatabase){
-		return true;
-	} else{
-		return false;
+	
+	public static String createEncodedPassword( String userPassword) {
+		BCryptPasswordEncoder encoder = new BCryptPasswordEncoder(12); // decides the length and strength of password
+		String Password = encoder.encode(userPassword); // encripting the string input from user
+		// System.out.println(Password);
+	
+		return Password;
 	}
-  }
+	
+	public static boolean confirmPassword(String passwordFromUser, String passwordFromDatabase){
+		
+		return BCrypt.checkpw(passwordFromUser,passwordFromDatabase);
+	}
+
 }
+
+
+ 
